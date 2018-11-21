ad2c3a5c9698eca43d7b71038a9b9bb9b9a88abc
Lawson Caudill <lawson@caudillclan.us>
2018-11-21 18:19:01 -0300

    Created shell for Identity Server 4 implementation.

---
+++ HundredProof.Identity.Server.Owin.Console/ConsoleAbstraction.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConsoleAbstraction.cs" company="HundredProof">
+//    Copyright (c) HundredProof 2018. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace HundredProof.Identity.Server.Owin.Console
+{
+    /// <summary>
+    /// Abstraction layer for the console harness bootstrapper.
+    /// </summary>
+    public class ConsoleAbstraction : ConsoleAbstractionBase
+    {
+    }
+}
