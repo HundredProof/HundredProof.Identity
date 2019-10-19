20160ba7ae3b7359adff9c2c93373dce0904d9eb
The Dave Abides <dcampbell@spectresolutions.net>
2019-10-19 00:30:11 -0500

    branching to add IDserver4

---
+++ HundredProof.Identity.Server.Owin.Console/OAuthProviderModels/GoogleApi.cs
@@ -0,0 +1,27 @@
+﻿namespace HundredProof.Identity.Server.Owin.Console.OAuthProviderModels
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class GoogleApi
+    {
+        [JsonProperty("client_id")] public string ClientId { get; set; }
+
+        [JsonProperty("project_id")] public string ProjectId { get; set; }
+
+        [JsonProperty("auth_uri")] public Uri AuthUri { get; set; }
+
+        [JsonProperty("token_uri")] public Uri TokenUri { get; set; }
+
+        [JsonProperty("auth_provider_x509_cert_url")]
+        public Uri AuthProviderX509CertUrl { get; set; }
+
+        [JsonProperty("client_secret")] public string ClientSecret { get; set; }
+
+        [JsonProperty("redirect_uris")] public Uri[] RedirectUris { get; set; }
+    }
+}
